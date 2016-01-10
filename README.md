421970d30b04783e85d90504822d67e98a6230e6
Jason von Nieda <jason@vonnieda.org>
2016-01-09 19:25:53 -0800

    Initial checkin. Working well but not ready for release until platform binaries are included.

---
+++ README.md
@@ -0,0 +1,25 @@
+# libuvc4j
+
+A "batteries included" libuvc binding for Java.
+
+* Runtime support by [Bridj](https://github.com/nativelibs4java/BridJ).
+* API generated with [JNAerator](https://github.com/nativelibs4java/JNAerator).
+* Bundled libusb and libuvc binaries, packaged by [OpenPnP](http://openpnp.org). 
+
+## Maven
+ 
+
+## Rebuilding
+
+Originally generated using:
+
+`java -jar jnaerator/target/jnaerator-0.13-SNAPSHOT-shaded.jar -library uvc /usr/local/include/libuvc/libuvc.h -o ../openpnp/libuvc4j -v -mode Maven -mavenGroupId org.openpnp -mavenArtifactId libuvc4j -package org.openpnp.libuvc4j`
+
+## Notes
+
+### libuvc on Mac with Homebrew
+
+Installing libuvc via brew results in a library that doesn't find manufacturer
+and product names. In addition, it is not configured with JPEG support by
+default. Installing it with `brew install libuvc --HEAD --width-jpeg` resolves
+these issues.
---
+++ src/test/java/org/openpnp/libuvc4j/test/TestEnumerateCameras.java
@@ -0,0 +1,40 @@
+package org.openpnp.libuvc4j.test;
+
+import org.bridj.Pointer;
+import org.junit.Test;
+import org.openpnp.libuvc4j.UvcLibrary;
+import org.openpnp.libuvc4j.UvcLibrary.uvc_context;
+import org.openpnp.libuvc4j.UvcLibrary.uvc_device;
+import org.openpnp.libuvc4j.uvc_device_descriptor;
+
+public class TestEnumerateCameras {
+	@Test
+	public void test() {
+		Pointer<Pointer<uvc_context>> ctx = Pointer.allocatePointer(uvc_context.class);
+		System.out.println("uvc_init " + UvcLibrary.uvc_init(ctx, null));
+		Pointer<Pointer<Pointer<uvc_device>>> devList = Pointer.allocatePointerPointer(uvc_device.class);
+		System.out.println("uvc_get_device_list " + UvcLibrary.uvc_get_device_list(ctx.get(), devList));
+		for (Pointer<uvc_device> dev : devList.get()) {
+			if (dev == null) {
+				break;
+			}
+			Pointer<Pointer<uvc_device_descriptor>> desc = Pointer.allocatePointer(uvc_device_descriptor.class);
+			System.out.println("uvc_get_device_descriptor " + UvcLibrary.uvc_get_device_descriptor(dev, desc));
+			System.out.println("  Vendor ID " + desc.get().get().idVendor());
+			System.out.println("  Product ID " + desc.get().get().idProduct());
+			System.out.println("  Manufacturer " + getString(desc.get().get().manufacturer()));
+			System.out.println("  Product " + getString(desc.get().get().product()));
+			System.out.println("  Serial " + getString(desc.get().get().serialNumber()));
+			
+			UvcLibrary.uvc_free_device_descriptor(desc.get());
+		}
+		UvcLibrary.uvc_free_device_list(devList.get(), (byte) 1);
+	}
+	
+	public static String getString(Pointer<Byte> p) {
+		if (p == null) {
+			return null;
+		}
+		return p.getCString();
+	}
+}
